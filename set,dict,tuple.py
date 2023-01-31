44a3e5f8c5089bd447f6a5c30cf9466a5fd38230
Deta1l <57856724+Deta1l@users.noreply.github.com>
2023-01-31 13:17:39 +0300

    Update set,dict,tuple.py

--- set,dict,tuple.py
+++ set,dict,tuple.py
@@ -63,4 +63,35 @@ dict.values() - возвращает значения в словаре.
 
 print("<------------set---------->")
 
+a = set('hello')
+print(a)
 
+a2 = {i ** 2 for i in range(10)} # генератор множеств
+print(a2)
+
+words = ['hello', 'daddy', 'hello', 'mum']
+print(set(words))
+
+'''
+len(s) - число элементов в множестве (размер множества).
+x in s - принадлежит ли x множеству s.
+set.isdisjoint(other) - истина, если set и other не имеют общих элементов.
+set == other - все элементы set принадлежат other, все элементы other принадлежат set.
+set.issubset(other) или set <= other - все элементы set принадлежат other.
+set.issuperset(other) или set >= other - аналогично.
+set.union(other, ...) или set | other | ... - объединение нескольких множеств.
+set.intersection(other, ...) или set & other & ... - пересечение.
+set.difference(other, ...) или set - other - ... - множество из всех элементов set, не принадлежащие ни одному из other.
+set.symmetric_difference(other); set ^ other - множество из элементов, встречающихся в одном множестве, но не встречающиеся в обоих.
+set.copy() - копия множества.
+------------
+set.update(other, ...); set |= other | ... - объединение.
+set.intersection_update(other, ...); set &= other & ... - пересечение.
+set.difference_update(other, ...); set -= other | ... - вычитание.
+set.symmetric_difference_update(other); set ^= other - множество из элементов, встречающихся в одном множестве, но не встречающиеся в обоих.
+set.add(elem) - добавляет элемент в множество.
+set.remove(elem) - удаляет элемент из множества. KeyError, если такого элемента не существует.
+set.discard(elem) - удаляет элемент, если он находится в множестве.
+set.pop() - удаляет первый элемент из множества. Так как множества не упорядочены, нельзя точно сказать, какой элемент будет первым.
+set.clear() - очистка множества.
+'''
