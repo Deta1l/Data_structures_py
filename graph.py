589cd567591131c9e8b90658a81e38a585f86bf4
Deta1l <57856724+Deta1l@users.noreply.github.com>
2023-03-15 01:03:32 +0300

    3

--- graph.py
+++ graph.py
@@ -52,4 +52,46 @@ def obhod_v_shirinu(x,f):
 #obhod_v_shirinu(1, graph_1)
 
 #--------------------------------------------------------
+#алгоритм дейкстры
+'''
+class Graph:
+    def __init__(self, edges, n):
+        self.adjList = [None] * n
+        
+        for i in range(n):
+            self.adjList[i] = []
+        for (src, dest, weight) in edges:
+            self.adjList[src].append((dest, weight))
+'''
+ 
+ #--------------------------------------------------------
+ #гамильтонов цикл
 
+G = {0:[1],
+    1:[0, 2], 
+    2:[1, 3], 
+    3:[2, 4, 5], 
+    4:[3, 6], 
+    5:[3], 
+    6:[4]
+}
+
+def hamilton(graph, start_v):
+    size = len(graph)
+
+    to_visit = [None, start_v]
+    path = []
+    while(to_visit):
+        v = to_visit.pop()
+        if v : 
+            path.append(v)
+            if len(path) == size:
+                break
+            for x in set(graph[v])-set(path):
+                to_visit.append(None) # out
+                to_visit.append(x) # in
+        else: # if None we are comming back and pop v
+            path.pop()
+    return path
+
+print(hamilton(G,6))
