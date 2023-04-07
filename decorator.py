7e8e67ce042700a459064db111b492c07c803f38
Deta1l <57856724+Deta1l@users.noreply.github.com>
2023-04-07 19:27:13 +0300

    3

---
+++ decorator.py
@@ -0,0 +1,16 @@
+def decorator_function(func):
+    def wrapper():
+        print('Функция-обёртка!')
+        print('Оборачиваемая функция: {}'.format(func))
+        print('Выполняем обёрнутую функцию...')
+        func()
+        print('Выходим из обёртки')
+    return wrapper
+
+@decorator_function    
+def aa():
+    a = 2
+    a = a * 2
+    print(a)
+    
+aa()
